d04e5bae315dec18f5c63d327c33571cce6eeb0c
ZeroDevs <usamau32o@gmail.com>
2023-02-14 16:15:50 +0200

    Update names.py

--- names.py
+++ names.py
@@ -1,1 +1,56 @@
-names = [    {        "name": "Ash",        "info": "(-0,1) "    },    {        "name": "Frost",        "info": "(-0,1)"    },    {        "name": "Sledge ",        "info": "(-0,1)"    },    {        "name": "blackbeard ",        "info": "(-0,1)"    },    {        "name": "Captio",        "info": "(-0,1)"    }, { "name": "Tachanka ",        "info": "(-0,1)"},{ "name": "Castle ",        "info": "(-0,1)"},{ "name": "Thatcher  ",        "info": "(-0,1)"},{ "name": "iq  ",        "info": "(-0,1)"},{ "name": "Thermite",        "info": "(-0,1)"},{ "name": "Caviera",      "info": "(-0,1)"},{ "name": "KapKan",      "info": "(-0,1)"},{ "name": "Pulse",      "info": "(-0,1)"} ]
+data = {
+    "names": [
+        {
+            "name": "Ash",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Frost",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Sledge ",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Blackbeard",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Captio",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Tachanka",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Castle",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Thatcher",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "IQ",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Thermite",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Caviera",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Kapkan",
+            "info": "(-0,1)"
+        },
+        {
+            "name": "Pulse",
+            "info": "(-0,1)"
+        }
+    ]
+}
